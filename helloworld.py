471dc438d68d62e5e56d3693b96c2c71315092ac
BesimAltnok <besimaltnok@gmail.com>
2016-02-13 15:26:45 +0200

    Create helloworld.py

---
+++ helloworld.py
@@ -0,0 +1,10 @@
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/besim')
+def page():
+	return "<center><h2>http://www.besimaltinok.com</h2></center>"
+
+if __name__ == "__main__":
+	app.run(debug=True, port=1337)
+	
