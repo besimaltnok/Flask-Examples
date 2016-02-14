973db752d871bbdd1335a1ccf2a337b7dab8a5e0
BesimAltnok <besimaltnok@gmail.com>
2016-02-14 21:54:04 +0200

    Create f.py

---
+++ f.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+# -*- coding : utf-8 -*-
+
+import os
+import sys
+
+
+static = ['css', 'js', 'img']
+
+try:
+	proje = sys.argv[1]
+	if proje:
+		for i in static:
+			os.makedirs(proje+"/static/"+i)
+		os.mkdir(proje+"/template")
+		os.system("touch "+ proje+"/"+proje+'.py')
+
+except IndexError:
+		print "For use  : ./f.py proje_name"
